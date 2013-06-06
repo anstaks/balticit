c9172ae328fac6632d822502e2b007981ee30146
anstak <anstak@gmail.com>
2013-06-06 10:25:34 +0300

    Form for content page activeadmin

--- app/admin/pages.rb
+++ app/admin/pages.rb
@@ -1,5 +1,32 @@
+# Encoding: utf-8
 ActiveAdmin.register Page do
-  index do
+  scope :all, :default => true
+  scope :no_seo
 
+  index do
+    column :id
+    column 'Заголовок', :title
+    column 'Псевдоним',:alias, sortable:false
+    column :description, sortable:false
+    column :keywords, sortable:false do |key|
+      array = key.keywords.split(',')
+      div style:'color:#090' do
+        array.count.to_s + ' keywords'
+      end
+    end
+    default_actions
   end
+
+  form do |f|
+    f.inputs "Details" do
+      f.input :title
+      f.input :alias, :label => "Псевдоним"
+      f.input :description
+    end
+    f.inputs "Content" do
+      f.input :content, :as => :html_editor, :input_html => { :class => 'autogrow', :rows => 10, :cols => 20, :maxlength => 10  }
+    end
+    f.actions
+  end
+
 end
--- app/models/page.rb
+++ app/models/page.rb
@@ -1,3 +1,5 @@
 class Page < ActiveRecord::Base
   attr_accessible :alias, :content, :description, :keywords, :title
+
+  scope :no_seo, where(keywords:'',description:'')
 end

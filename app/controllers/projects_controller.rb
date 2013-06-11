d1bc0e8de21c34791e731a5d3c62ab968d915577
anstak <anstak@gmail.com>
2013-06-11 10:24:34 +0300

    Working background url for modal ajax

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,6 @@
 class ProjectsController < ApplicationController
   def show
     @project = Project.find(params[:id])
-    render json: @project, status: :ok
+    render json: @project.as_json(methods: :background_url), status: :ok
   end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,8 @@
 class Project < ActiveRecord::Base
-  attr_accessible :content, :title, :background_uid, :background, :retained_background, :preview_background, :description
+  attr_accessible :content, :title, :background_uid, :background, :preview_background, :description
   image_accessor :background
+
+  def background_url
+    self.background.url
+  end
 end

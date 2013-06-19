5a778cdeda54d23a6a861de80b2825c3b4d843b1
anstak <anstak@gmail.com>
2013-06-19 18:16:02 +0300

    Make different views

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,11 @@ class PagesController < ApplicationController
   def show
     @projects = Project.all
     @page = Page.find_by_slug(params[:slug]) || Page.find(params[:slug])
+
+    if File.exists?(Rails.root.join("app", "views", params[:controller], "#{params[:slug]}.html.erb"))
+      render params[:slug]
+    else
+      puts 'no'
+    end
   end
 end
